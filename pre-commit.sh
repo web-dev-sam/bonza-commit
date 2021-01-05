157ecc9d61e5761fd09b7d2fcb67c9ba0c6439da
Your Name <you@example.com>
2021-01-05 23:02:53 +0100

    Tested pre commit hook

--- pre-commit.sh
+++ pre-commit.sh
@@ -9,9 +9,15 @@ eslint -f visualstudio $FOLDER | grep -oP $CLEANER >> $TEMP_FILE
 pylint --msg-template='{path}({line}): error {msg_id} : {msg} [at {obj}] ' $FOLDER | grep -oP $CLEANER >> $TEMP_FILE
 
 if grep -q "): error " $TEMP_FILE; then
+    echo "Linter Error! Open git log to learn more!"
+    echo ''
+    echo "-------------------------------   LINTER ERRORS   -------------------------------"
+    echo ''
     cat $TEMP_FILE | grep -P --color $COLORING
-    rm $TEMP_FILE
+    echo ''
+    echo "-------------------------------   LINTER ERRORS   -------------------------------"
     echo ''
     echo 'Stopped Committing!'
-    exec 1>&2
+    rm $TEMP_FILE
+    exit 1
 fi 
