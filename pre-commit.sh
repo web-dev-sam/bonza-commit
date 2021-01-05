e758f551cabf656b7d1eb939fd965070e4a8dccb
Your Name <you@example.com>
2021-01-05 22:55:04 +0100

    Fixed Bug

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,12 +1,12 @@
 
+FOLDER="demo"
 TEMP_FILE=".log"
-CLEANER="(?<=$1/).*"
+CLEANER="(?<=$FOLDER/).*"
 COLORING="(error|warning)"
 
-
 > $TEMP_FILE
-eslint -f visualstudio $1 | grep -oP $CLEANER >> $TEMP_FILE
-pylint --msg-template='{path}({line}): error {msg_id} : {msg} [at {obj}] ' $1 | grep -oP $CLEANER >> $TEMP_FILE
+eslint -f visualstudio $FOLDER | grep -oP $CLEANER >> $TEMP_FILE
+pylint --msg-template='{path}({line}): error {msg_id} : {msg} [at {obj}] ' $FOLDER | grep -oP $CLEANER >> $TEMP_FILE
 
 if grep -q "): error " $TEMP_FILE; then
     cat $TEMP_FILE | grep -P --color $COLORING
